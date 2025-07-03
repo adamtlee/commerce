c2a5811043b8c014de341ba68e7003afa7fc3445
adamtlee <adam.lee346@gmail.com>
2025-07-03 13:10:31 -0600

    updated user seeder information

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,9 +5,9 @@ namespace Database\Seeders;
 use App\Models\User;
 use App\Models\Product;
 use App\Models\SaleItem;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,12 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $this->call(UserSeeder::class);
 
         // Create sample products with inventory
         $products = [
